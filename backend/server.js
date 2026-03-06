7ec5648ddcca060dc90a3bb5a0b9bf31d5761c18
Tavyan Vatts <tavyanvatts18@gmail.com>
2026-03-06 19:04:08 +0530

    Initial MeloGram backend setup with MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
-const express = require("express")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.send("MeloGram API running")
-})
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000")
-})
+    console.log("Server running on port 5000");
+});
